1fe097d0789f2b1eaaa85b547ddc43a47b290b1b
aabazary <85041715+aabazary@users.noreply.github.com>
2024-07-03 17:24:16 -0700

    Added Test File, as well as Classes to pass each of the given tests

---
+++ src/main/java/edu/sdccd/cisc191/ArrayConsumer.java
@@ -0,0 +1,17 @@
+package edu.sdccd.cisc191;
+
+public class ArrayConsumer {
+    private final String[] arr;
+    public ArrayConsumer(String[] stringArr) {
+        this.arr = stringArr;
+    }
+
+    public String consume() {
+        String value = arr[0];
+
+        for (int i=1; i<arr.length; i++) {
+            arr[i-1] = arr[i];
+        }
+        return value;
+    }
+}
---
+++ src/main/java/edu/sdccd/cisc191/ArrayListConsumer.java
@@ -0,0 +1,24 @@
+package edu.sdccd.cisc191;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayListConsumer
+{
+
+    private List<String> list;
+
+    public ArrayListConsumer(ArrayList<String> list)
+    {
+
+        this.list = list;
+
+    }
+
+    public String consume()
+    {
+
+        return list.remove(0);
+    }
+
+}
